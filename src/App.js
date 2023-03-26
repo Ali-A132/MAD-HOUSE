1bcf9f387abc78ac47ceb6b1c120ea0ecb7b511f
Ali Akbar <91800364+Ali-A132@users.noreply.github.com>
2023-03-25 22:11:46 -0400

    remove strict browser router, update router redirection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, 
+  Route, Routes,  Redirect, Link, BrowserRouter, useNavigate } from "react-router-dom";
+import conversion from './components/Stems/Stems';
+import Redirection from './components/Redirect/Redirect';
+import Interview from './components/Interview/Interview';
+import Concert from './components/Concert/Concert';
+import Unreleased from './components/Unreleased/Unreleased';
+import Stems from './components/Stems/Stems';
+import flipbook from "./images/giphyrecord.gif";
+import album from "./images/album.jpg";
+import madhouseLogo from "./images/madhouseLogo.png";
 import './App.css';
+import Home from './components/Home/Home';
+
+// Import the functions you need from the SDKs you need
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Routes>
+        <Route exact path = "/" element = {<Home/>}/>
+        <Route exact path = "/redirect" element = {<Redirection/>}></Route>
+        <Route exact path = "/interview" element = {<Interview/>}></Route>
+        <Route exact path = "/concert" element = {<Concert/>}></Route>
+        <Route exact path = "/unreleased" element = {<Unreleased/>}></Route>
+        <Route exact path = "/stems" element = {<Stems/>}></Route>
+      </Routes>
+    </section>
+
   );
 }
 
 export default App;
+
+// submit upload button
+{/* <input type="file" id="actual-btn"/>
+
+
+<label for="actual-btn"></label> */}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import redirect from './components/Redirect/Redirect';
+import Home from './components/Home/Home';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+)
+
+export {
+  Home
+};
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
