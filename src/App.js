4e088e8c10a5306eaa53d6191d68960e61021671
Ali Akbar <91800364+Ali-A132@users.noreply.github.com>
2023-04-27 02:59:30 -0400

    update route path for each respective page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ import Interview from './components/Interview/Interview';
 import Concert from './components/Concert/Concert';
 import Unreleased from './components/Unreleased/Unreleased';
 import Stems from './components/Stems/Stems';
+import RedirectLilas from './components/RedirectLilas/RedirectLilas';
+import RedirectNujabes from './components/RedirectNujabes/RedirectNujabes';
+import StemsNujabes from './components/StemsNujabes/StemsNujabes';
+import UnreleasedNujabes from './components/UnreleasedNujabes/UnreleasedNujabes';
+import ConcertNujabes from './components/ConcertNujabes/ConcertNujabes';
+import InterviewNujabes from './components/InterviewNujabes/InterviewNujabes';
+import InterviewLilas from './components/InterviewLilas/InterviewLilas';
+import UnreleasedLilas from './components/UnreleasedLilas/UnreleasedLilas';
+import ConcertLilas from './components/ConcertLilas/ConcertLilas';
+import StemsLilas from './components/StemsLilas/StemsLilas';
 import flipbook from "./images/giphyrecord.gif";
 import album from "./images/album.jpg";
 import madhouseLogo from "./images/madhouseLogo.png";
@@ -26,6 +36,16 @@ function App() {
         <Route exact path = "/concert" element = {<Concert/>}></Route>
         <Route exact path = "/unreleased" element = {<Unreleased/>}></Route>
         <Route exact path = "/stems" element = {<Stems/>}></Route>
+        <Route exact path = "/redirectLilas" element = {<RedirectLilas/>}></Route>
+        <Route exact path = "/redirectNujabes" element = {<RedirectNujabes/>}></Route>
+        <Route exact path = "/stemsNujabes" element = {<StemsNujabes/>}></Route>
+        <Route exact path = "/unreleasedNujabes" element = {<UnreleasedNujabes/>}></Route>
+        <Route exact path = "/interviewNujabes" element = {<InterviewNujabes/>}></Route>
+        <Route exact path = "/concertNujabes" element = {<ConcertNujabes/>}></Route>
+        <Route exact path = "/stemsLilas" element = {<StemsLilas/>}></Route>
+        <Route exact path = "/unreleasedLilas" element = {<UnreleasedLilas/>}></Route>
+        <Route exact path = "/interviewLilas" element = {<InterviewLilas/>}></Route>
+        <Route exact path = "/concertLilas" element = {<ConcertLilas/>}></Route>
       </Routes>
     </section>
 
